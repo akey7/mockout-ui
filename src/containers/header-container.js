cf39ef653ecb9e45cf4c9f7082614ba1dca7510d
Alicia Key <alicia@akey7.com>
2018-04-21 00:12:30 -0600

    Use destructuring.

--- src/containers/header-container.js
+++ src/containers/header-container.js
@@ -2,22 +2,14 @@ import React from 'react'
 import './header-container.css'
 import { connect } from 'react-redux'
 
-const Header = (props) => (
+const Header = ({ username }) => (
   <div className='header-container'>
     <a className='title' href='#title'>Mockout</a>
-    <a className='signin' href='#signin'>{ props.auth.username !== null ? props.auth.username : 'Sign In' }</a>
-    <a href='#logout'>{ props.auth.username !== null ? 'Logout' : '' }</a>
+    <a className='signin' href='#signin'>{ username !== null ? username : 'Sign In' }</a>
+    <a href='#logout'>{ username !== null ? 'Logout' : '' }</a>
   </div>
 )
 
-const mapStateToProps = (state) => {
-  const result = {
-    auth: state.auth
-  }
-
-  console.log(`AWWW shucks ${Object.keys(result.auth)}`)
-
-  return result
-}
+const mapStateToProps = (state) => state.auth
 
 export default connect(mapStateToProps)(Header)
