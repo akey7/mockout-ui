ab63d4c0f10b659f390db2c3b9a0e40099a7a816
Alicia Key <alicia@akey7.com>
2018-04-21 00:13:17 -0600

    Uneeded ternary.

--- src/containers/header-container.js
+++ src/containers/header-container.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 const Header = ({ username }) => (
   <div className='header-container'>
     <a className='title' href='#title'>Mockout</a>
-    <a className='signin' href='#signin'>{ username !== null ? username : 'Sign In' }</a>
+    <a className='signin' href='#signin'>{ username || 'Sign In' }</a>
     <a href='#logout'>{ username !== null ? 'Logout' : '' }</a>
   </div>
 )
