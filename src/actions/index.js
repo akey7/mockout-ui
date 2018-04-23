63c5cd7575ed71cb7721a56e1639461f906dd613
Alicia Key <alicia@akey7.com>
2018-04-22 21:36:54 -0600

    OK, it can be thunked.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
 /* eslint-disable no-unused-vars */
 
 export function loginSubmit(credentials) {
-  return {
-    type: 'LOGIN_SUBMIT',
-    payload: credentials
+  // return {
+  //   type: 'LOGIN_SUBMIT',
+  //   payload: credentials
+  // }
+
+  return (dispatch) => {
+    Promise.resolve(credentials)
+      .then((x) => dispatch({ type: 'LOGIN_SUBMIT', payload: x}))
   }
 }
 
