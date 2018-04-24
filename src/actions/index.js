c6f153671b2c5b9fc907e192c3eff3e0cb5abeff
Alicia Key <alicia@akey7.com>
2018-04-23 21:35:05 -0600

    Remove a comment.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,24 @@
 /* eslint-disable no-unused-vars */
 
-export function loginSubmit(credentials) {
-  // return {
-  //   type: 'LOGIN_SUBMIT',
-  //   payload: credentials
-  // }
+export function loginSubmit({ email, password }) {
+  const url = `${process.env.REACT_APP_API_URL}/login`
+
+  const opts = {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
 
   return (dispatch) => {
-    Promise.resolve(credentials)
-      .then((x) => dispatch({ type: 'LOGIN_SUBMIT', payload: x}))
+    fetch(url, opts)
+      .then((result) => result.json())
+      .then((result) => {
+        console.log(result)
+        dispatch({ type: 'LOGIN_SUBMIT', payload: dispatch })
+      })
   }
 }
 
