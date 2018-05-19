05f21552942beeaa2a1230c4993efa49bc11f8c1
Alicia Key <alicia@akey7.com>
2018-05-19 16:39:50 -0600

    Doesn't look at the isLoggedIn state.

--- src/containers/protect-route-container.js
+++ src/containers/protect-route-container.js
@@ -5,8 +5,8 @@ import LoginFormContainer from './login-form-container'
 
 // NOTE: This is a function component so I don't need to worry about
 // shadowing Component because I didn't destructure it in an import.
-const ProtectRoute = ({ component: Component, isLoggedIn, ...rest }) => {
-  if (isLoggedIn) {
+const ProtectRoute = ({ component: Component, token, isLoggedIn, ...rest }) => {
+  if (token) {
     return <Route render={ (props) => <Component {...props} /> } />
   }
   else {
