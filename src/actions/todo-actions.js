680f1528f0b3c20b69ce620143eb07f7903ec0ce
Alicia Key <alicia@akey7.com>
2018-05-19 17:10:08 -0600

    The begining of a read for all the todos

---
+++ src/actions/todo-actions.js
@@ -0,0 +1,14 @@
+// const readTodos = ({ token }) => {
+//   const url = `${process.env.REACT_APP_API_URL}/todo`
+//
+//   const opts = {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json',
+//       'Authorization': `Bearer ${token}`
+//     }
+//   }
+//
+//
+// }
