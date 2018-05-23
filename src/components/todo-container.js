4bcdf9f05066b24fae05c47e7841dc7da04a092f
Alicia Key <alicia@akey7.com>
2018-05-23 00:37:02 -0600

    Edit toggle going.

--- src/components/todo-container.js
+++ src/components/todo-container.js
@@ -6,11 +6,24 @@ import { bindActionCreators } from 'redux'
 class Todo extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      editing: false
+    }
+
     this.delTodo = this.delTodo.bind(this)
+    this.onSwitchToEdit = this.onSwitchToEdit.bind(this)
   }
 
   delTodo() {
     this.props.deleteTodo({ id: this.props.id, token: this.props.auth.token })
+  }
+
+  onSwitchToEdit() {
+    this.setState({
+      ...this.state,
+      editing: true
+    })
   }
 
   render() {
@@ -18,7 +31,9 @@ class Todo extends Component {
     const item = todos[id].item
 
     return (<div>
-      <button onClick={ this.delTodo }>DELETE</button>
+      {!this.state.editing
+        && <span><button onClick={this.onSwitchToEdit}>EDIT</button><button onClick={this.delTodo}>DELETE</button></span>
+      }
       <span>{id}: {item}</span>
     </div>)
   }
